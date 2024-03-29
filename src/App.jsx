dab4d142c0ea9a83c6d21b171e074e73b7101ee5
Bishwas Pandey <pandeybishwas5@gmail.com>
2024-03-29 22:02:55 +1100

    Add results table

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import Header from "./components/Header.jsx"
-import UserInput from "./components/UserInput.jsx"
+import { useState } from "react";
+
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 500,
+    expectedReturn: 6,
+    duration: 5,
+});
+
+
+const inputIsValid = userInput.duration >= 1;
+
+function handleChange(id, value){
+    setUserInput((prevValue) => ({
+        ...prevValue,
+        [id]: +value,
+    }))
+}
+
+
   return (
     <>
     <Header />
-    <UserInput />
+    <UserInput userInput={userInput} onChange={handleChange}/>
+    {!inputIsValid && <p className="center">Please enter a duration greater than 0.</p>}
+    {inputIsValid && <Results input={userInput}/>}
     </>
     
   )
